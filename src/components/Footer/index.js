cdec4bfd4a5c25c9abefa6d28366956d8e1ea357
Joanna Lin <chiayilin94@gmail.com>
2018-11-12 16:48:37 -0800

    add components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+  <div className="footer">
+    <div className="container">
+      <hr className="border-primary" />
+      <p>
+        <a href="http://data.naturalcapitalproject.org/nightly-build/invest-users-guide/html/index.html">
+          <br />
+          <strong>InVEST User Guide</strong>
+        </a>
+      </p>
+    </div>
+  </div>
+);
+
+export default Footer;
---
+++ src/components/Navi/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+class Navi extends React.Component {
+  render() {
+    const { title } = this.props;
+    return (
+      <nav className="navbar navbar-expand flex-column flex-md-row bg-primary">
+        <div className="container">
+          <h1 className="navbar-brand mb-0">{title}</h1>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navi;
