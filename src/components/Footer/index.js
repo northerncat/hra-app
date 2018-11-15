79798be96ce0fe7119b4de99db54093e8ae4061c
Joanna Lin <chiayilin94@gmail.com>
2018-11-14 17:14:51 -0800

    use className selector to decorate navbar and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import './style.css';
 class Footer extends React.Component {
   render() {
     return (
-      <div className="footer col-xs-12 col-sm-12 col-md-12 col-lg-12" id="footer">
+      <div className="footer col-xs-12 col-sm-12 col-md-12 col-lg-12">
           <h3> <a href="http://data.naturalcapitalproject.org/nightly-build/invest-users-guide/html/index.html"
             target="blank">InVEST User Guide</a> </h3>
       </div>
