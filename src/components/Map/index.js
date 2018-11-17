0520117e9402f228fe1aa65f3eb958d9ad3f57e5
Joanna Lin <chiayilin94@gmail.com>
2018-11-16 23:27:04 -0800

    zoom to layer

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import { Map, TileLayer, LayersControl, ScaleControl, GeoJSON } from "react-leaflet";
+import React, { Component } from 'react';
+import { Map, TileLayer, LayersControl, ScaleControl, GeoJSON } from 'react-leaflet';
+import { bbox } from '@turf/turf'
 import 'leaflet/dist/leaflet.css';
 // import L from 'leaflet/dist/leaflet.js';
 import './style.css';
@@ -14,10 +15,12 @@ export default class hraMap extends Component {
     this.state = {
     lat: 49.016,
     lng: -126.131,
-    zoom: 10,
     maxZoom: 10, // for ESRI Ocean Base Map, which has the most limited zoom level
     minZoom: 2, // global scale
     vectors: [],
+    lats: [],
+    lngs: [],
+    maxBbox: [[-90, -180], [90, 180]],
     };
 
     const self = this;
@@ -30,8 +33,15 @@ export default class hraMap extends Component {
             let vectorPath = vectorDir + '/' + response.data[i];
             fetch(vectorPath)
               .then(response => response.json()) // parsing the data as JSON
-              .then(data => self.setState(
-                {vectors: [...self.state.vectors, data]}))
+              .then(data => {
+                self.setState({vectors: [...self.state.vectors, data]});
+
+                // Add geojson bounding box to lat and long arrays
+                let vectorBbox = bbox(data);
+                self.setState({lngs: [...self.state.lngs, vectorBbox[0], vectorBbox[2]]});
+                self.setState({lats: [...self.state.lats, vectorBbox[1], vectorBbox[3]]});
+                self.getMaxBbox();
+              })
           };
         },
         error => console.log(error));
@@ -45,8 +55,28 @@ export default class hraMap extends Component {
     }
   }
 
+  getMaxBbox() {
+    if (this.state.lats.length > 0 && this.state.lngs.length > 0) {
+      // calc the min and max lng and lat
+      var minlat = Math.min(...this.state.lats),
+          maxlat = Math.max(...this.state.lats);
+      var minlng = Math.min(...this.state.lngs),
+          maxlng = Math.max(...this.state.lngs);
+      this.setState({maxBbox: [[minlat, minlng],[maxlat, maxlng]]});
+    }
+  }
+
   renderGeoJSONs() {
     let vectors = this.state.vectors;
+    // if (vectors.length > 0) {
+    //   vectors.map(vectorData => {
+    //     return (
+    //       <Overlay key={vectorData.name} name={vectorData.name} checked>
+    //         <GeoJSON key={vectorData.name} data={vectorData} style={this.getStyle()}/>
+    //       </Overlay>
+    //     );
+    //   });
+    // }
     if (vectors.length > 0) {
       return (
         vectors.map(vectorData => (
@@ -54,34 +84,36 @@ export default class hraMap extends Component {
             <GeoJSON key={vectorData.name} data={vectorData} style={this.getStyle()}/>
           </Overlay>
         ))
-      )
+      );
     }
   }
 
+  // center={position} zoom={this.state.zoom}
   render() {
     let position = [this.state.lat, this.state.lng];
     return (
       <div>
-        <Map center={position} zoom={this.state.zoom} style={{height: "650px"}} maxZoom={this.state.maxZoom} minZoom={this.state.minZoom}>
+        <Map id='mapdiv' maxZoom={this.state.maxZoom} minZoom={this.state.minZoom} bounds={this.state.maxBbox}>
 
-          <LayersControl position='topright' collapsed="false">
-            <BaseLayer name="Open Street Map" checked>
+          <LayersControl position='topright' collapsed='false'>
+            <BaseLayer name='Open Street Map' checked>
               <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors" />
+                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                attribution='&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors' />
             </BaseLayer>
 
-            <BaseLayer name="ESRI Ocean Basemap">
+            <BaseLayer name='ESRI Ocean Basemap'>
               <TileLayer
-                url="https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}"
-                attribution="Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri"
-                maxZoom="10"/>
+                url='https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}'
+                attribution='Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri'
+                maxZoom='10'/>
             </BaseLayer>
 
             {this.renderGeoJSONs()}
+
           </LayersControl>
 
-          <ScaleControl position={"bottomleft"} maxWidth={100}/>
+          <ScaleControl position={'bottomleft'} maxWidth={100}/>
         </Map>
       </div>
     );
